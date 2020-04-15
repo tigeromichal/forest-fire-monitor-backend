0f1d7de407a76cd1be12242c4a0d7d00d974487b
tigeromichal <empiasecki96@gmail.com>
2020-04-15 22:11:25 +0200

    Initialized Spring Boot application

---
+++ src/main/java/com/piasecki/adach/forestfiremonitorbackend/ForestFireMonitorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.piasecki.adach.forestfiremonitorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForestFireMonitorBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForestFireMonitorBackendApplication.class, args);
+    }
+
+}
